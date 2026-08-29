7a4bd3c0d6e4e128f9c8c4519707a324508e0fc5
Arpit Tyagi <arpittyagi389@gmail.com>
2026-08-29 17:34:54 +0530

    make header

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,104 @@
 import "./App.css";
 import photo from "./assets/photo.png";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaFolder,
+  FaDownload,
+} from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
 function App() {
   return (
     <>
       <header className="header">
-        <div className="header-logo">Portfolio</div>
+        <div className="temp">
+          <div className="header-logo">AT</div>
+
+          <div className="header-info">
+            <div className="header-name">Arpit Tyagi</div>
+            <div className="header-profile">JAVA DEVELOPER</div>
+          </div>
+        </div>
         <nav className="header-link">
           <a href="#home">Home</a>
           <a href="#about">About</a>
-          <a href="#skills">Skill</a>
+          <a href="#skills">Skills</a>
           <a href="#education">Education</a>
-          <a href="#project">Project</a>
-          <a href="#contact">Contact</a>
-          <a href="#resume">Resume</a>
+          <a href="#project">Projects</a>
+          <a href="#github">GitHub</a>
+          <a href="#contact" className="contact">
+            Contact
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1MY0lbLKFHBJHDKX5iJdyZV_Ib3zrsED8/view"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="resume-btn"
+          >
+            <div className="both">
+              <div>
+                <FaDownload />
+              </div>{" "}
+              Resume
+            </div>
+          </a>
         </nav>
       </header>
       <main className="main">
+        <p className="opp">
+          🟢 Open to full-time opportunities and internships
+        </p>
         <section id="home">
           <div className="intro">
-            <p className="greeting">Hi 👋, I'm</p>
+            <p className="greeting">Hello, I'm 👋</p>
             <h1>Arpit Tyagi</h1>
             <h2>Java Full Stack Developer</h2>
-            <p>
-              Building scalable backend systems and modern web applications.
-              <br />
-              Passionate about building scalable applications, writing clean and
-              efficient code, and solving real-world problems with modern
-              technologies.
+            <p className="me">
+              <p>
+                Building scalable applications with{" "}
+                <span className="highlight">
+                  Java, Spring Boot & Microservices
+                </span>
+                . Skilled in{" "}
+                <span className="highlight">
+                  Kafka, Redis, REST APIs & JWT Security
+                </span>
+                . Experienced with{" "}
+                <span className="highlight">React.js, PostgreSQL & Docker</span>
+                . Strong in{" "}
+                <span className="highlight">
+                  DSA, problem-solving & full-stack development
+                </span>
+                . Passionate about writing{" "}
+                <span className="highlight">clean, maintainable code</span> and
+                building reliable systems.
+              </p>
             </p>
-            <div className="contact-info">
-              <span>
-                <strong>✉</strong> arpittyagi389@gmail.com
-              </span>
 
-              <span>
-                <strong>☎</strong> +91 8445970389
-              </span>
-
-              <span>
-                <strong>📍</strong> Meerut, Uttar Pradesh, India
-              </span>
+            <div className="home-link">
+              <a
+                href="https://drive.google.com/file/d/1MY0lbLKFHBJHDKX5iJdyZV_Ib3zrsED8/view"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="resume-bt"
+              >
+                <div className="home-both-1">
+                  <div>
+                    <FaDownload />
+                  </div>
+                  Download Resume
+                </div>
+              </a>
+              <a href="#project">
+                <div className="home-both-2">
+                  <div>
+                    <FaFolder />
+                  </div>
+                  View Projects
+                </div>
+              </a>
             </div>
 
             <div className="social-links">
@@ -50,7 +108,7 @@ function App() {
                 rel="noreferrer"
                 className="linkedin"
               >
-                LinkedIn ↗
+                <FaLinkedin />
               </a>
 
               <a
@@ -59,7 +117,7 @@ function App() {
                 rel="noreferrer"
                 className="github"
               >
-                GitHub ↗
+                <FaGithub />
               </a>
 
               <a
@@ -68,11 +126,17 @@ function App() {
                 rel="noreferrer"
                 className="leetcode"
               >
-                LeetCode ↗
+                <SiLeetcode />
+              </a>
+
+              <a href="mailto:arpittyagi389@gmail.com" className="email-link">
+                <FaEnvelope />
               </a>
             </div>
           </div>
-          <img src={photo} alt="Arpit Tyagi" className="profile-pic" />
+          <div className="profile-pic">
+            <img src={photo} alt="Arpit Tyagi" className="profile-pic" />
+          </div>
         </section>
         <section id="about">
           <h3>About</h3>
@@ -99,95 +163,79 @@ function App() {
 
         <section id="skills">
           <h3>Tech Skills</h3>
-          <div className="frontend" >
-            <h4>Frontend</h4>
-            <ul >
-              <li>HTML5</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>React.js</li>
-            </ul>
+          <div className="skill">
+            <div className="lang">
+              <h4>Coding languages</h4>
+              <ul>
+                <li>Java</li>
+                <li>Java Scipt</li>
+                <li>Python</li>
+              </ul>
             </div>
-            <div className="backend">
-            <h4>Backend</h4>
-            <ul>
-              <li>Java</li>
-              <li>Spring Boot</li>
-              <li>MicroServices</li>
-              <li>Spring Security</li>
-              <li>Spring AI</li>
-              <li>JPA / Hibernate</li>
-              <li>Redis</li>
-              <li>Kafka</li>
-            </ul>
+            <div className="lang">
+              <h4>Frontend</h4>
+              <ul>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React.js</li>
+              </ul>
             </div>
-            <div className="devops">
-            <h4>Devops/Tools</h4>
-            <ul>
-              <li>Docker</li>
-              <li>Git & GitHub</li>
-              <li>CI/CD (basic)</li>
-              <li>Postman</li>
-              <li>IntelliJ IDEA</li>
-              <li>Eclipse</li>
-              <li>VS Code</li>
-            </ul>
+            <div className="lang">
+              <h4>Backend</h4>
+              <ul>
+                <li>Java</li>
+                <li>Spring Boot</li>
+                <li>MicroServices</li>
+                <li>Spring Security</li>
+                <li>Spring AI</li>
+                <li>JPA / Hibernate</li>
+                <li>Redis</li>
+                <li>Kafka</li>
+              </ul>
             </div>
-            <div>
-            <h4>Coding languages :</h4>
-            <ul>
-              <li>Java</li>
-              <li>Java Scipt</li>
-              <li>Python</li>
-            </ul>
+            <div className="lang">
+              <h4>Devops/Tools</h4>
+              <ul>
+                <li>Docker</li>
+                <li>Git & GitHub</li>
+                <li>CI/CD (basic)</li>
+                <li>Postman</li>
+                <li>IntelliJ IDEA</li>
+                <li>Eclipse</li>
+                <li>VS Code</li>
+              </ul>
             </div>
-            <div>
-            <h4>Cloud Computing</h4>
-            <p>
-              AWS Cloud (EC2 , IAM , CloudWatch , S3 , IaC, Cloud migration)
-            </p>
+            <div className="lang">
+              <h4>Cloud & AI</h4>
+              <ul>
+                <li>
+                  AWS Cloud (EC2 , IAM , CloudWatch , S3 , IaC, Cloud migration)
+                </li>
+                <li>Prompt Engineering</li>
+                <li>Basic Understanding of LLM Models</li>
+                <li>Basic Understanding of RAG</li>
+              </ul>
             </div>
-            <div>
-            <h4>AI</h4>
-            <ul>
-              <li>Prompt Engineering</li>
-              <li>Basic Understanding of LLM Models</li>
-              <li>Basic Understanding of RAG</li>
-            </ul>
-            </div>
+          </div>
         </section>
-        <section>
+        <section className="soft">
           <h3>Soft Skill</h3>
-          <p>Strong DSA problem-solving mindset (450+ LeetCode)</p>
-          <p>Comfortable owning a feature end-to-end (backend + frontend)</p>
-          <p>
-            Clear technical communication — documents APIs and system design
-            decisions
-          </p>
-        </section>
-        <section id="resume">
-          <h3>Resume</h3>
-          <a
-            href="https://drive.google.com/file/d/1MY0lbLKFHBJHDKX5iJdyZV_Ib3zrsED8/view?usp=sharing"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Download Resume
-          </a>
+          <div className="soft-skill">
+            <p> • Strong DSA problem-solving mindset (450+ LeetCode)</p>
+            <p>
+              • Comfortable owning a feature end-to-end (backend + frontend)
+            </p>
+            <p>
+              • Clear technical communication — documents APIs and system design
+              decisions
+            </p>
+          </div>
         </section>
         <section id="project">
           <h3>Projects</h3>
           <div className="project">
-            <h4>
-              PulseStreamAI{" "}
-              <a
-                href="https://github.com/ArpitTyagi0001/pulsestream-ai"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>
-            </h4>
+            <h4>PulseStreamAI</h4>
             <p>
               AI-Powered Real-Time Event Monitoring System built with Spring
               Boot, Apache Kafka, Redis, Spring AI, Spring Security (JWT),
@@ -219,18 +267,14 @@ function App() {
                 Redis) with Docker Compose for single-command deployment.
               </li>
             </ul>
-          </div>
-          <div>
-            <h4>
-              Hospital Management System{" "}
-              <a
-                href="https://github.com/ArpitTyagi0001/hospital-management-system-backend"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>
-            </h4>
+            <a
+              href="https://github.com/ArpitTyagi0001/pulsestream-ai"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            <h4>Hospital Management System</h4>
             <p>
               Secure backend system built with Spring Boot, Spring Security
               (JWT), JPA/Hibernate, PostgreSQL, REST APIs, and Maven.
@@ -253,9 +297,16 @@ function App() {
                 JPA/Hibernate lazy loading.
               </li>
             </ul>
+            <a
+              href="https://github.com/ArpitTyagi0001/hospital-management-system-backend"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
           </div>
         </section>
-        <section>
+        <section className="cert">
           <h3>Certifications & Achievements</h3>
           <ul>
             <li>
@@ -279,27 +330,48 @@ function App() {
             method="POST"
             encType="multipart/form-data"
           >
-            <div>
+            <div className="name">
               <label htmlFor="name">Name : </label>
               <input type="text" id="name" name="name" required />
             </div>
             <br></br>
-            <div>
+            <div className="email">
               <label htmlFor="email">Email : </label>
               <input type="email" id="email" name="email" required />
             </div>
             <br></br>
-            <div>
+            <div className="msg">
               <label htmlFor="msg">Message : </label>
               <textarea id="msg" name="msg" required />
             </div>
             <br></br>
-            <div>
+            <div className="attachment">
               <label htmlFor="attachment">Attachment (optional) :</label>
               <input type="file" id="attachment" name="attachment" />
             </div>
-            <button type="submit">Send</button>
+            <button type="submit" className="button">
+              Send
+            </button>
           </form>
+        </section>
+        <section id="github">
+          <div className="text">
+            <div>OPEN SOURCE</div>
+            <div>GitHub Portfolio</div>
+            <p>
+              My GitHub portfolio is a collection of my projects, code, and
+              hands-on work in Java, Spring Boot, and React. Explore my
+              repositories to see backend systems, full-stack applications,
+              APIs, and real-world development projects.
+            </p>
+          </div>
+          <div className="logo">
+            <FaGithub />
+            <div className="github-profile">
+              <div>ArpitTyagi0001</div>
+              <div>Java Full Stack Developer</div>
+            </div>
+          </div>
         </section>
       </main>
       <footer className="footer">
